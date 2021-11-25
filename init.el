79f7fb18530a05de5acab8197a3c208ba9ccf0f8
bingxinliu <bingxin.liu@wustl.edu>
2021-11-25 15:45:34 -0600

    update

--- init.el
+++ init.el
@@ -107,6 +107,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
+ '(whitespace-tab ((t (:foreground "#636363")))))
 
 
